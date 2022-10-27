30205c18e1100f12a7a84388d0251839153762b3
Oliver Train <olivertrain1996@gmail.com>
2022-10-27 17:11:26 +0100

    Add changes to the images

--- src/components/NavBar/Events.js
+++ src/components/NavBar/Events.js
@@ -8,7 +8,7 @@ function EventCard() {
       {MotorsportData.map((item, index) => {
         return (
           <div >
-          <a style={{backgroundImage: item.image}} href="#" class="block p-6 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-8 w-72 test">
+          <a href="#" class="block p-6 max-w-sm rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 m-8 w-4/5 test">
             <img src={item.image}></img>
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-white">{item.title}</h5>
               <p class="font-normal text-gray-700 dark:text-gray-400">HELLLL YEAHHHH</p>
