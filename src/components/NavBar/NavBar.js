5b77d8140d755201cbf7706c9ebd7eca82996f8f
Oliver Train <otrain@goodwood.local>
2022-11-10 15:22:59 +0000

    Added to navbar.js added in the new slider and also allowed for it to be closed depending on the state changing

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,8 +5,12 @@ import { MenuData } from "./MenuData";
 import EventCard from "./Events";
 import "./NavBar.css";
 import MenuDropDown from "./MenuDropDown";
-import { HorseRacingData, MotorsportData, StayEatData } from "../EventData";
-import OpenBookButton from "./BookNow/BookNow";
+import { HorseRacingData, MotorsportData } from "../EventData";
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import ShopTiles from "./ShopTiles";
+
 
 function NavBar() {
   const [sidebar, setSidebar] = useState(false);
@@ -15,14 +19,17 @@ function NavBar() {
     setSidebar(!sidebar);
   };
 
-  const [shopButton, setShopButton] = useState();
+  const [open, setOpen] = useState(true)
+  const buyButton = () => {
+    setOpen(!open)
+  }
 
-  const showShopBar = () => {
-    setShopButton(!shopButton);
+  const test = () => {
+    console.log("Clicked")
   }
 
   return (
-    <>
+    <div>
       <div className="navbar">
         <div>
           {/* BURGER DIV */}
@@ -39,7 +46,7 @@ function NavBar() {
           <button className="bg-black border-2 w-20 m-3 text-base border-custom-green text-white">
             Shop
           </button>
-          <button onClick={showShopBar} className="bg-custom-green w-20 m-3 text-lg text-black">
+          <button onClick={buyButton} className="bg-custom-green w-20 m-3 text-lg text-black">
             Book
           </button>
           <i className="fa-solid fa-magnifying-glass text-2xl text-white m-3"></i>
@@ -81,6 +88,65 @@ function NavBar() {
                 </button>
               </div>
             </div>
+            <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <div className="fixed inset-0" />
+
+        <div className="fixed inset-0 overflow-hidden">
+          <div className="absolute inset-0 overflow-hidden">
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex bg-bg-custom h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                    <div className="px-4 sm:px-6">
+                      <div className="flex items-start justify-between">
+                        <Dialog.Title className="text-lg font-medium text-white">Shop</Dialog.Title>
+                        <div className="ml-3 flex h-7 items-center">
+                          <button
+                            type="button"
+                            className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            onClick={() => setOpen(false)}
+                          >
+                            <span className="sr-only">Close panel</span>
+                            <XMarkIcon className="h-6 w-6 text-white bg-bg-custom" aria-hidden="true" />
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                      <div className="absolute inset-0 px-4 sm:px-6">
+                      {/* BELOW IS OUR CODE */}
+                      <div className="text-white flex justify-between text-lg border-t">
+                        <button className="font-light hover:font-bold active:font-bold">Tickets</button>
+                        <button className="font-light hover:font-bold active:font-bold">Expierences</button>
+                        <button className="font-light hover:font-bold active:font-bold">Restaurants</button>
+                        <button className="font-light hover:font-bold active:font-bold">Hotel</button>
+                      </div>
+                        
+                        
+                        
+                      {/* ABOVE IS OUR CODE*/}
+                        <div className="h-full  mt-8 border-gray-200" aria-hidden="true">
+                          <ShopTiles />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </div>
+      </Dialog>
+          </Transition.Root>
           <div className="text-3xl text-white w-full overflow-x-auto">
 
             <EventCard data={MotorsportData} />
@@ -102,7 +168,7 @@ function NavBar() {
         </div>
       </nav>
 
-    </>
+    </div>
   );
 }
 export default NavBar;
